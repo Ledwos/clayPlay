7a4db4b415968a0c7fe22f3fffa1ffe96c268369
Ledwos <32643853+Ledwos@users.noreply.github.com>
2020-11-25 10:57:11 +0000

    icons not working

--- clay-front/src/components/form/Clayform.js
+++ clay-front/src/components/form/Clayform.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ClayForm, {ClayInput} from '@clayui/form';
+import ClayIcon, {ClayIconSpriteContext} from '@clayui/icon';
+
 
 // import clayUI
 import '@clayui/css/lib/css/atlas.css';
 
 const Clayform = () => {
+    const [active, setActive] = useState(false);
+    const iconPath = '';
     return (
-        <div className='container'>
-            <div className='sheet'>
-                <p>Clay form will be built in this comp</p>
+        <div >
+            <div className='container'>
+                <div className='sheet'>
+                    <p>Clay form will be built in this comp (below)</p>
+                </div>
+            </div>
+            <div className='sheet text-left'>
+                <ClayForm>
+                    <ClayForm.Group className='form-group-sm'>
+                        <label htmlFor='basicInput'>Name</label>
+                        <ClayInput placeholder='Name' type='text' />
+                        <ClayForm.FeedbackGroup>
+                            <ClayForm.FeedbackItem>
+                                <ClayForm.FeedbackIndicator
+                                    spritemap={iconPath}
+                                    symbol='check-circle-full'
+                                />
+                                {'I\'m the feedback item'}
+                            </ClayForm.FeedbackItem>
+                        </ClayForm.FeedbackGroup>
+                    </ClayForm.Group><ClayForm.Group className='form-group-sm'>
+                        <label htmlFor='basicInput'>Description</label>
+                        <textarea className='form-control' placeholder='Description' />
+                    </ClayForm.Group>
+                </ClayForm>
             </div>
         </div>
     );
